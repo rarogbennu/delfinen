de500bf28b62c341e8599975c3a808692b11d063
Rolf <rarogbennu@gmail.com>
2023-05-25 14:33:58 +0200

    removed unused code

--- js/helpers.js
+++ js/helpers.js
@@ -1,10 +1,7 @@
 // Importer funktioner og variabler fra andre moduler
-import { updateClicked, deleteClicked, updateResultatClicked } from "./script.js";
-import { sortData, showData} from "./data-handling.js";
-import { endpoint } from "./rest-services.js";
-
-// Definér en global variabel for sidestørrelse
-const pageSize = 105;
+import {updateClicked, deleteClicked} from "./script.js";
+import {sortData, showData} from "./data-handling.js";
+import {endpoint} from "./rest-services.js";
 
 // Funktion til at forberede data - konverterer et objekt til et array
 function prepareData(dataObject) {
@@ -19,19 +16,7 @@ function prepareData(dataObject) {
       }
     }
   }
-
   return dataArray;
-}
-
-// Funktioner til at vise og skjule sidens knapper
-function showPaginationButtons() {
-  const pageButtonContainer = document.getElementById("page-buttons");
-  pageButtonContainer.style.display = "block";
-}
-
-function hidePaginationButtons() {
-  const pageButtonContainer = document.getElementById("page-buttons");
-  pageButtonContainer.style.display = "none";
 }
 
 // Funktion til at oprette en beholder med redigerings- og sletningsknapper
@@ -54,35 +39,7 @@ function createButtonContainer(item) {
   return buttonContainer;
 }
 
-// Funktion til at oprette sidens knapper
-function createPageButtons(data) {
-  const totalPages = Math.ceil(data.length / pageSize);
-
-  // Få den eksisterende pageButtonContainer, hvis den findes
-  let pageButtonContainer = document.getElementById('page-buttons');
-
-  // Hvis pageButtonContainer ikke findes, opret den
-  if (!pageButtonContainer) {
-    pageButtonContainer = document.createElement('div');
-    pageButtonContainer.id = 'page-buttons';
-  } else {
-    // Hvis den findes, fjern dens indhold
-    pageButtonContainer.innerHTML = '';
-  }
-
-  for (let i = 1; i <= totalPages; i++) {
-    const pageButton = document.createElement('button');
-    pageButton.textContent = i.toString();
-    pageButton.addEventListener('click', () => {
-      window.currentPage = i;
-      showData(data, window.currentPage);
-    });
-    //pageButtonContainer.appendChild(pageButton);  // Tilføj knappen til beholderen
-  }
-
-  // Tilføj beholderen til medlemmer-sektionen
-  document.getElementById('medlemmer').appendChild(pageButtonContainer);
-}function searchData() {
+function searchData() {
   // Nulstil nuværende side
   window.currentPage = 1;
   const searchField = document.getElementById("searchField");
@@ -91,7 +48,6 @@ function createPageButtons(data) {
   fetch(`${endpoint}/medlemmer.json`)
     .then((response) => response.json())
     .then((data) => {
-      console.log("Data:", data); // Console log data
       if (data) {
         const medlemmer = prepareData(data); // Konverter data til et array ved hjælp af prepareData
 
@@ -107,9 +63,6 @@ function createPageButtons(data) {
 
         window.allData = filteredData; // Gem de filtrerede data
 
-        // Opret side-nummer knapper
-        createPageButtons(window.allData);
-
         // Vis den første side efter at have hentet og filtreret dataene
         showData(window.allData, window.currentPage);
 
@@ -120,8 +73,6 @@ function createPageButtons(data) {
       }
     });
 }
-
-
 
   function capitalizeFirstLetter(str) {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
@@ -140,4 +91,4 @@ function createPageButtons(data) {
   }
 
 
-export { searchData, createButtonContainer, prepareData, capitalizeFirstLetter, closeDialog };
+export {searchData, createButtonContainer, prepareData, capitalizeFirstLetter, closeDialog};
