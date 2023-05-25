42467c5d0bb7c1de249dfd78009468d7cedaec74
Rolf <rarogbennu@gmail.com>
2023-05-25 14:04:00 +0200

    Removed unused code

--- js/views.js
+++ js/views.js
@@ -1,7 +1,6 @@
 import {medlemOptions, enableStævneInput, enablePlaceringInput, generateResultatTable, enableStævneInputUpdate, enablePlaceringInputUpdate} from "./resultater.js"
 
 function initViews() {
-    console.log("initViews");
     window.addEventListener("hashchange", changeView);
     changeView();
 }
@@ -14,14 +13,12 @@ function changeView() {
   }
 
   hideViews();
-  hidePaginationButtons();
 
   document.querySelector(hashLink).classList.add("active");
   setActiveLink(hashLink);
 
   if (hashLink === "#medlemmer") {
     console.log("#medlemmer")
-    showPaginationButtons();
   }
   else if (hashLink === "#opret-medlem") {
     console.log("#opret-medlem")
@@ -44,21 +41,6 @@ function changeView() {
   }
 }
 
-
-function hidePaginationButtons() {
-    const paginationButtons = document.querySelectorAll(".page-buttons");
-    paginationButtons.forEach(button => {
-        button.style.display = "none";
-    });
-}
-
-function showPaginationButtons() {
-    const paginationButtons = document.querySelectorAll(".page-buttons");
-    paginationButtons.forEach(button => {
-        button.style.display = "block";
-    });
-}
-
 function setActiveLink(view) {
     const link = document.querySelector(`a.show-link[href="${view}"]`);
     if (link) {
@@ -70,8 +52,5 @@ function hideViews() {
     document.querySelectorAll(".show-content").forEach(link => link.classList.remove("active"));
     document.querySelectorAll(".show-link").forEach(link => link.classList.remove("active"));
 }
-
-
-
 
 export {initViews};
